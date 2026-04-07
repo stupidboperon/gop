27047b455b4d285353101f718655d1f34e7b2c24
stupidboperon <jamesurba11@stratk12.org>
2026-04-07 12:45:11 -0400

    oopsy

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(bool hardReset){
     VDP_loadTileSet(img_cloudybg.tileset,ind,DMA);
     ind+=img_cloudybg.tileset->numTile;
     TileMap *bgmap = unpackTileMap(img_cloudybg.tilemap,NULL);
-    VDP_setTileMapEx(BG_A, bgmap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, bgind), 0, 0,  0, 0, 10, 19, CPU);
+    VDP_setTileMapEx(BG_A, bgmap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, bgind), 0, 0,  0, 0, 40, 28, CPU);
     PAL_setColors(0,(u16*)img_cloudybg.palette->data,16,CPU);
     
     //VDP_drawText("beebo", 0, 0);
